987f6e1e7212205afa1057154625249fe5a7cba3
aaaaryaaa <aarya.masters@gmail.com>
2025-09-07 00:31:24 +0530

    added harish

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Select an option:"
-echo "1) Start server (nginx only)"
-echo "2) Stop server (full shutdown)"
+echo "1) Start server"
+echo "2) Stop server"
 read -p "Enter choice [1 or 2]: " choice
 
 case "$choice" in
   1)
-    echo "Starting nginx only..."
-    docker-compose up -d nginx
+    echo "Starting server..."
+    docker-compose up -d
     ;;
   2)
     echo "Stopping and removing all containers + volumes..."
